83e37d780f6d1bad34ea75abc38dc18a6850b3d7
RRRobert212 <robertwd1@gmail.com>
2024-04-11 17:31:13 -0400

    added weapons, armor, consumable lists

--- constants.py
+++ constants.py
@@ -8,8 +8,14 @@ enemy_list = ["Goblin", "Ghoul"]
 enemy_health = [[30,40], [20,30]]
 enemy_attack = [[10,20],[20,30]]
 
-weapon_types = ["Punch"]
+weapon_types = ["Punch", "old stick"]
 weapon_damage_factor = [[0.175,0.275]]
+
+armor_types = []
+armor_block_factor = [[]]
+
+consumable_types = []
+consumbale_effect_factor = [[]]
 
 
 
