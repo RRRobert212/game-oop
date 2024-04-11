341cc6303a155539bef3cab5f9bf4bf9daafd967
RRRobert212 <robertwd1@gmail.com>
2024-04-11 19:09:20 -0400

    testing combat flow and inventory

--- main.py
+++ main.py
@@ -16,44 +16,51 @@ def main():
     e = Enemy.spawn_ghoul() #probably update the spawn function so it automatically puts them on the map with spawn_entity
     #but if I want to do that I prob have to add the map as a parameter to the spawn function
 
-    m = map.Map(10,10)
-   # m.display()
-    m.spawn_entity(c, 7,4)
-    m.spawn_entity(e, 2,2)
+    c.inventory.add_weapon(0)
+    c.inventory.add_weapon(1)
 
-    m.spawn_wall(3,3)
-    m.spawn_wall(3,4)
-    m.spawn_wall(3,5)
+    e.inventory.add_weapon(0)
 
-    m.spawn_wall(0,0)
-    m.spawn_wall(9,9)
 
-    m.display()
+    combat_flow(c,e)
+
+#     m = map.Map(10,10)
+#    # m.display()
+#     m.spawn_entity(c, 7,4)
+#     m.spawn_entity(e, 2,2)
+
+#     m.spawn_wall(3,3)
+#     m.spawn_wall(3,4)
+#     m.spawn_wall(3,5)
+
+#     m.spawn_wall(0,0)
+#     m.spawn_wall(9,9)
+
+#     m.display()
 
 
     
-    #this eventually needs to get written as a function on its own, basically the whole gameplay
-    while True:
+#     #this eventually needs to get written as a function on its own, basically the whole gameplay
+#     while True:
 
-        i = input("WASD: ")
-        print("\n\n\n\n\n\n\n")
+#         i = input("WASD: ")
 
-        if i == "w":
-            m.move_up(c)
-        elif i == "a":
-            m.move_left(c)
-        elif i == "s":
-            m.move_down(c)
-        elif i == "d":
-            m.move_right(c)
-        elif i == "x": return #exit input, for testing
-        else: print("Invalid input.")
+#         if i == "w":
+#             m.move_up(c)
+#         elif i == "a":
+#             m.move_left(c)
+#         elif i == "s":
+#             m.move_down(c)
+#         elif i == "d":
+#             m.move_right(c)
+#         elif i == "x": return #exit input, for testing
+#         else: print("Invalid input.")
 
 
         
-        if m.collision_detection(c,e):
-            combat_flow(c,e)
-            m.display()
+#         if m.collision_detection(c,e):
+#             combat_flow(c,e)
+#             m.display()
             
         
 
