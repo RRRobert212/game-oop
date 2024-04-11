f76cd1ba69cff382708098780a3afc45d9e41ac7
RRRobert212 <robertwd1@gmail.com>
2024-04-11 19:12:19 -0400

    replaced punch function with generic attack function

--- actions.py
+++ actions.py
@@ -17,16 +17,16 @@ class Actions:
         if target.is_alive():
             print(str(target.get_name()) + " received " + str(amount) + " damage! They have " + str(target.get_health()) + " health remaining.")
         else: print(f"{target.get_name()} reveived {amount} damage! They have no health remaining!")
+        print()
         
 
 
-    #this can be made better by turning it into a general attack function, only difficult thing is print statement but even that can be done with a list
     @staticmethod
-    def punch(user, target): 
+    def attack(user, target, weapon_id): 
         """generic weak attack function, user argument punches target argument"""
-        print(str(user.get_name()) + " used punch! ")
+        print(str(user.get_name()) + " attacked " + str(target.get_name() +" with their " + str(weapon_types[weapon_id])))
 
-        damage = round(weapon_damage_roll(0) * user.get_attack())
+        damage = round(weapon_damage_roll(weapon_id) * user.get_attack())
 
         Actions.receive_damage(target, damage)
 
@@ -36,7 +36,6 @@ class Actions:
         """heals target health by given amount, target and amount as input, prints statement about it"""
         target.set_health(target.get_health() + amount)
         print(str(target.get_name()) + " healed for " + str(amount) + " health! They now have " + str(target.get_health()) + " health.")
-
 
 
 
